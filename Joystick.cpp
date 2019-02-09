c2473836d1df0a1871738d6710f4137af233ccfb
NaOH <31431753+NaOHSol@users.noreply.github.com>
2019-02-10 01:00:04 +0800

    Update Joystick.cpp

--- Joystick.cpp
+++ Joystick.cpp
@@ -5,36 +5,35 @@
 *********************************************************/
 
 #include "Joystick.h"
-Joystick::Joystick(int X_pin, int Y_pin, int X_middle_value, int Y_middle_value, int dead_zone, bool Reverse_X, bool Reverse_Y)
+Joystick::Joystick(int X_pin, int Y_pin, int X_middle_value, int Y_middle_value, int dead_zone, bool reverse_X, bool reverse_Y)
 {
   _X_pin = X_pin;
   _Y_pin = Y_pin;
   _X_middle_value = X_middle_value;
   _Y_middle_value = Y_middle_value;
   _dead_zone = dead_zone;
-  _Reverse_X = Reverse_X;
-  _Reverse_Y = Reverse_Y;
+  _reverse_X = reverse_X;
+  _reverse_Y = reverse_Y;
 }
 
 void Joystick::P_Joystick()
 {
   _X_buf = analogRead(_X_pin);
   _Y_buf = analogRead(_Y_pin);
-  
+
   _X_buf -= _X_middle_value;
   _Y_buf -= _Y_middle_value;
   if(_Reverse_X)_X_buf=-_X_buf;
   if(_Reverse_Y)_Y_buf=-_Y_buf;
-  
+
   _absX = abs(_X_buf);
   _absY = abs(_Y_buf);
-  
-  
+
   if (_absX < _dead_zone && _absY < _dead_zone)return;
-  
+
   _hY = _absX * 6;
   _lY = _absX / 6;
-  
+
   if(_Y_buf > 0 && _X_buf > 0 && _absY > _lY && _absY < _hY)(*left_up)();
   else if(_Y_buf > 0 && _X_buf < 0 && _absY > _lY && _absY < _hY)(*right_up)();
   else if(_Y_buf < 0 && _X_buf > 0 && _absY > _lY && _absY < _hY)(*left_down)();
